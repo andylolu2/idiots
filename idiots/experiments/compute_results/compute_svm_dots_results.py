d54492800c678e77c4e53f61208c4817114b9e7b
DeeproChoudhury <Dchoudhury47@gmail.com>
2024-03-01 17:16:01 +0000

    s5 experiments

--- idiots/experiments/compute_results/compute_svm_dots_results.py
+++ idiots/experiments/compute_results/compute_svm_dots_results.py
@@ -68,9 +68,9 @@ logs_base_path = "/home/dc755/idiots/logs/"
 
 experiments = [
                #("mnist", "mnist-64", "checkpoints/mnist/checkpoints", "classification", 40, 2000, 512, 64, 512),
-              #  ("div", "div", "checkpoints/division/exp21/checkpoints", "grokking", 1000, 50_000, 512, 512, 512),
-               ("div_mse", "div_mse", "checkpoints/exp22/division_mse/checkpoints", "grokking", 1000, 50_000, 512, 512, 512),
-               ("s5", "s5", "checkpoints/s5/exp24/checkpoints", "grokking", 1000, 50_000, 512, 512, 512)
+               ("div", "div-256", "checkpoints/division/exp21/checkpoints", "grokking", 1000, 50_000, 256, 256, 256),
+               ("div_mse", "div_mse-256", "checkpoints/division_mse/exp22/checkpoints", "grokking", 1000, 50_000, 256, 256, 256),
+              #  ("s5", "s5-512", "checkpoints/s5/exp24/checkpoints", "grokking", 1000, 50_000, 512, 512, 512)
               ]
 
 for experiment_name, experiment_json_file_name, experiment_path, experiment_type, step_distance, total_steps, num_dots_samples, num_svm_training_samples, num_svm_test_samples in experiments:
